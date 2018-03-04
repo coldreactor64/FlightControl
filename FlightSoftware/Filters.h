a5649369e05dca516ef53f6ece588a413321b833
Jeremy Hall <Coldreactor64@gmail.com>
2018-03-04 10:02:11 -0600

    Wrong files listed for some reason

--- FlightSoftware/Filters.h
+++ FlightSoftware/Filters.h
@@ -1,10 +1,10 @@
 
 #include <Arduino.h>
-void MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy,
-                              float gz, float mx, float my, float mz,
+void MadgwickQuaternionUpdate(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy,
+                              int16_t gz, float mx, float my, float mz,
                               float deltat);
-void MahonyQuaternionUpdate(float ax, float ay, float az, float gx, float gy,
-                            float gz, float mx, float my, float mz,
+void MahonyQuaternionUpdate(int16_t Iax, int16_t Iay, int16_t Iaz, int16_t Igx, int16_t Igy,
+                            int16_t Igz, float mx, float my, float mz,
                             float deltat);
 const float * getQ();
 const float * getYPR();
